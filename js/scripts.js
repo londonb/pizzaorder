af465f04925cb4bf8c4b8e8a8812953ae99840e6
Brad London <london.brad@gmail.com>
2016-01-29 10:03:48 -0800

    respec the first spec to remove array option

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
-function Pizza(toppings, pizzaSize, cost) {
-  this.toppings = [];
+function Pizza(toppings, pizzaSize) {
+  this.toppings = toppings;
   this.pizzaSize = pizzaSize;
   this.cost = 0;
+}
+
+Pizza.prototype.giveTotal = function() {
+  this.cost = this.toppings.length * .25 + this.pizzaSize;
+  console.log(this.toppings);
+  return this.cost;
+}
+
+var sizePrice = function(currentPizza) {
+  if (currentPizza === "small") {
+    currentPizza.pizzaSize = 10;
+  } else if (currentPizza === "medium") {
+    currentPizza.pizzaSize = 12;
+  } else if (currentPizza === "large") {
+    currentPizza.pizzaSize = 14;
+  } else if (currentPizza === "xlarge") {
+    currentPizza.pizzaSize = 16;
+  } else {
+    return false;
+  }
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,16 @@
 describe ('Pizza', function() {
   it("creates an empty pizza order ready to be filled out with a cost of zero", function() {
   var testPizza = new Pizza(0,0);
-  expect(testPizza.toppings).to.eql([]);
+  expect(testPizza.toppings).to.equal(0);
   expect(testPizza.pizzaSize).to.equal(0);
   expect(testPizza.cost).to.equal(0);
+  });
+
+  it("will total the cost of a pizza order", function() {
+    var testPizza = new Pizza("large");
+    console.log(testPizza);
+    giveTotal(['pepperoni','sausage'],testPizza);
+    console.log(sizePrice);
+    expect(testPizza.giveTotal()).to.equal(14.50);
   });
 });
